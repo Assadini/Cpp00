5eba73c09e6765baa9ec8bd575055e461337024b
Taha Assadini <118551860+Assadini@users.noreply.github.com>
2025-05-13 14:40:32 +0000

    init

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cctype>
+
+int main(int ac,char **av)
+{
+    std::string str;
+    
+    if(ac==1)  
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *"<< std::endl;
+    }
+    else
+    {
+        for(int i=1;i<ac;i++){
+            str = av[i];
+            for(int j=0;j < str.length();j++){
+                str[j]=std::toupper(str[j]);
+                std::cout<< str[j];
+            }
+        }
+        std::cout<<std::endl;
+    }
+}
